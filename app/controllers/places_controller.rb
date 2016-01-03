68daa4acfb3fa413822b17af30406487314ee140
Mike Stone <michaeljacobstone@gmail.com>
2016-01-03 05:51:24 +0000

    Destory and edit not allowed

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-	before_action :authenticate_user!, :only => [:new, :create]
+	before_action :authenticate_user!, :only => [:new, :create, :edit, :update, :destory]
 	def index
 		@places = Place.all.paginate(page: params[:page], per_page: 5)
 	end
@@ -10,6 +10,9 @@ class PlacesController < ApplicationController
     
     def edit
     	@place = Place.find(params[:id])
+    	if @place.user != current_user
+    		return render :text => 'Not Allowed', :status => :forbidden
+  		end
     end
     
     def create
@@ -23,6 +26,9 @@ class PlacesController < ApplicationController
     
     def update
   		@place = Place.find(params[:id])
+  		if @place.user != current_user
+    		return render :text => 'Not Allowed', :status => :forbidden
+  		end
   		@place.update_attributes(place_params)
   		redirect_to root_path
   	end
